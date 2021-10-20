16409cf000515755a9f29b5d92d262f4a192975e
Oluwasegun Kesington <iamkesington@gmail.com>
2021-10-21 01:24:03 +0200

    Initialize Stack

--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -1,9 +1,11 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { Bucket } from "@aws-cdk/aws-s3";
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new Bucket(this, "FirstBuck", { versioned: true });
     // The code that defines your stack goes here
   }
 }
